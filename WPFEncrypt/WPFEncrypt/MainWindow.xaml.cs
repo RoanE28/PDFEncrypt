66753517c5219e0202c09e4782148888592c1164
Roan Eriksson <reriksson@live.co.za>
2021-09-07 21:02:44 +0200

    Itext check in

--- WPFEncrypt/WPFEncrypt/MainWindow.xaml.cs
+++ WPFEncrypt/WPFEncrypt/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using iText.Kernel.Pdf;
+
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +26,50 @@ namespace WPFEncrypt
         public MainWindow()
         {
             InitializeComponent();
+            Encrypt();
+        }
+
+        public void Encrypt()
+        {
+            string sourcePdf = @"E:\Temp\boardingPass.pdf";
+            //Passowrd the pwd for PDF security                 
+            string destPdf = @"E:\Temp\securedboardingPass.pdf";
+            if (File.Exists(destPdf))
+                File.Delete(destPdf);
+
+            using (Stream input = new FileStream(sourcePdf, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                PdfReader reader = new PdfReader(input);
+                //PdfDocument pdfDocTemp = new PdfDocument(reader);
+                //var permissions = (int)reader.GetPermissions();
+                // sourcePdf  unsecured PDF file
+                //destPdf secured PDF file
+
+                using (Stream output = new FileStream(destPdf, FileMode.Create, FileAccess.Write, FileShare.None))
+                {
+
+
+
+                    string Password = "abc@123";
+
+                    //var properties = new EncryptionProperties();
+
+                    var userPassword = Encoding.ASCII.GetBytes(Password);
+
+                    WriterProperties props = new WriterProperties().SetStandardEncryption(userPassword, null, EncryptionConstants.ALLOW_PRINTING, EncryptionConstants.STANDARD_ENCRYPTION_128);// | EncryptionConstants.DO_NOT_ENCRYPT_METADATA
+                    PdfWriter writer = new PdfWriter(output, props);
+                    PdfDocument pdfDoc = new PdfDocument(reader, writer);
+                    pdfDoc.Close();
+
+                    //properties.SetStandardEncryption(userPassword, null, permissions, (int)EncryptionConstants.STANDARD_ENCRYPTION_128);
+
+
+                    //PdfEncryptor.Encrypt(reader, output, properties, null);
+                    //PdfEncryptor.Encrypt(reader, output, true, Password, Password, PdfWriter.ALLOW_PRINTING);
+                }
+            }
         }
     }
+
+    
 }
